744126522b183ac16049b664ecf6fec6abbcdc71
niv810 <72561213+niv810@users.noreply.github.com>
2021-07-18 18:50:54 +0300

    len_docs was fixed

--- creation.py
+++ creation.py
@@ -5,6 +5,7 @@ from nltk.stem import PorterStemmer
 import sys
 import math
 
+total_docs = 0
 ir = {}
 len_docs = {}
 d = {"ir": ir, "len_docs": len_docs}
@@ -47,22 +48,32 @@ def corpus(doc):
 
 
 def add_docs_from_files(path):
+    global total_docs
     for i in range(74, 80):
         tree = ET.parse(f"{path}/cf{i}.xml")
         root = tree.getroot()
         for doc in root.findall(".//RECORD"):
+            total_docs += 1
             v = corpus(doc)
             doc_id = doc.find("./RECORDNUM")
-            d["len_docs"][doc_id.text] = len(v)  # need to fix
+#            d["len_docs"][doc_id.text] = len(v)  # need to fix
             max_tf = max(v.values())
             for term in v:
-                if term not in d["ir"]:
-                    d["ir"][term] = [0, {}]
-                d["ir"][term][0] += 1
-                d["ir"][term][1][doc_id.text] = v[term]/max_tf
-    total_docs = len(d["len_docs"])  # need to fix
-    for term in d["ir"]:
-        d["ir"][term][0] = math.log2(total_docs/d["ir"][term][0])  # need to fix
+                if term not in ir:
+                    ir[term] = [0, {}]
+                ir[term][0] += 1
+                ir[term][1][doc_id.text] = v[term]/max_tf
+#    total_docs = len(d["len_docs"])  # need to fix
+    for term in ir:
+        ir[term][0] = math.log2(total_docs/ir[term][0])
+        idf = ir[term][0]
+        for num_doc in ir[term][1]:
+            tf = ir[term][1][num_doc]
+            if num_doc not in len_docs:
+                len_docs[num_doc] = 0
+            len_docs[num_doc] += (idf * tf)**2
+    for doc in len_docs:
+        len_docs[doc] = math.sqrt(len_docs[doc])
 
 
 def inverted_index(path):
